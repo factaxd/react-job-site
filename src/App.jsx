3d371e6c4499abf538a2f0827ddc5ecfe2506306
İsmail Yarar <ismileyarar@gmail.com>
2024-10-06 12:46:04 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import HomeCards from './components/HomeCards'
+import JobListings from './components/JobListings'
+
+
+const App = () => {
+  return (
+    <>
+    <Navbar/>
+    <Hero/>
+    <HomeCards/>
+    <JobListings/>
+
+   
+
+      <section className="m-auto max-w-lg my-10 px-6">
+        <a
+          href="jobs.html"
+          className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+          >View All Jobs</a
+        >
+      </section>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/JobListings.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import jobsData from '../jobs.json'; // Assuming you have this in the correct relative path
+import JobListing from './JobListing';
+
+const JobListings = () => {
+  // Extract the jobs array from the imported JSON data
+  const { jobs } = jobsData;
+
+  // Ensure the jobs array is available before rendering
+  if (!Array.isArray(jobs)) {
+    return <p>No jobs available</p>;
+  }
+
+  return (
+    <section className="bg-blue-50 px-4 py-10">
+      <div className="container-xl lg:container m-auto">
+        <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
+          Browse Jobs
+        </h2>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+          {jobs.map((job) => (
+            <JobListing key={job.id} job={job}/>
+
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default JobListings;
